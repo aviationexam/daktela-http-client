a26889a54de19a692a79ea10cac704e75e77c7a7
Jan Trejbal <jan.trejbal@aviationexam.com>
2022-03-02 19:03:07 +0100

    Add IDaktelaHttpClient.GetAsync

--- src/Daktela.HttpClient/Implementations/DaktelaHttpClient.cs
+++ src/Daktela.HttpClient/Implementations/DaktelaHttpClient.cs
@@ -1,4 +1,8 @@
 using Daktela.HttpClient.Interfaces;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Daktela.HttpClient.Implementations;
 
@@ -6,15 +10,50 @@ public class DaktelaHttpClient : IDaktelaHttpClient
 {
     private readonly System.Net.Http.HttpClient _httpClient;
 
+    private readonly bool _leaveHttpClientOpen;
+
     public DaktelaHttpClient(
-        System.Net.Http.HttpClient httpClient
+        System.Net.Http.HttpClient httpClient,
+        bool leaveHttpClientOpen = false
     )
     {
         _httpClient = httpClient;
+        _leaveHttpClientOpen = leaveHttpClientOpen;
     }
 
     public void Dispose()
     {
-        _httpClient.Dispose();
+        if (!_leaveHttpClientOpen)
+        {
+            _httpClient.Dispose();
+        }
+    }
+
+    public async Task<T> GetAsync<T>(IHttpResponseParser httpResponseParser, string uri, CancellationToken cancellationToken)
+    {
+        var uriObject = new Uri(uri, UriKind.Relative);
+
+        using var httpRequestMessage = CreateHttpRequestMessage(HttpMethod.Get, uriObject);
+
+        var httpResponse = await _httpClient
+            .SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+            .ConfigureAwait(false);
+
+        var response = await httpResponseParser.ParseResponseAsync<T>(httpResponse.Content, cancellationToken);
+
+        return response;
+    }
+
+    private HttpRequestMessage CreateHttpRequestMessage(
+        HttpMethod method, Uri uri
+    ) => new(method, uri);
+
+    private HttpRequestMessage CreateHttpRequestMessage<TBody>(
+        HttpMethod method, Uri uri, TBody? body
+    ) where TBody : class
+    {
+        var httpMessage = CreateHttpRequestMessage(method, uri);
+
+        return httpMessage;
     }
 }
--- src/Daktela.HttpClient/Interfaces/IDaktelaHttpClient.cs
+++ src/Daktela.HttpClient/Interfaces/IDaktelaHttpClient.cs
@@ -1,7 +1,14 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Daktela.HttpClient.Interfaces;
 
 public interface IDaktelaHttpClient : IDisposable
 {
+    Task<T> GetAsync<T>(
+        IHttpResponseParser httpResponseParser,
+        string uri,
+        CancellationToken cancellationToken
+    );
 }
