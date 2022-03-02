2ec6120c9a74f6ff3a246fd1bdc598f47eda4962
Jan Trejbal <jan.trejbal@aviationexam.com>
2022-03-02 19:03:45 +0100

    Introduce HttpResponseParser

---
+++ src/Daktela.HttpClient/Implementations/HttpResponseParser.cs
@@ -0,0 +1,33 @@
+﻿using Daktela.HttpClient.Configuration;
+using Daktela.HttpClient.Implementations.JsonParsers;
+using Daktela.HttpClient.Interfaces;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Daktela.HttpClient.Implementations;
+
+public class HttpResponseParser : IHttpResponseParser
+{
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+    public HttpResponseParser(IOptions<DaktelaOptions> daktelaOptions)
+    {
+        _jsonSerializerOptions = new JsonSerializerOptions();
+        _jsonSerializerOptions.Converters.Add(new DateTimeOffsetParser(daktelaOptions));
+    }
+
+    public async Task<T> ParseResponseAsync<T>(
+        HttpContent httpResponseContent, CancellationToken cancellationToken
+    )
+    {
+        await using var responseStream = await httpResponseContent.ReadAsStreamAsync(cancellationToken);
+
+        var parsedObject = await JsonSerializer.DeserializeAsync<T>(responseStream, _jsonSerializerOptions, cancellationToken);
+
+        return parsedObject ?? throw new NullReferenceException(nameof(parsedObject));
+    }
+}
---
+++ src/Daktela.HttpClient/Interfaces/IHttpResponseParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Daktela.HttpClient.Interfaces;
+
+public interface IHttpResponseParser
+{
+    Task<T> ParseResponseAsync<T>(
+        HttpContent httpResponseContent, CancellationToken cancellationToken
+    );
+}
