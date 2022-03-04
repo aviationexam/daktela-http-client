2691a93f9f30a321e6230dd08d54bc900e47490f
Jan Trejbal <jan.trejbal@aviationexam.com>
2022-03-04 12:00:19 +0100

    Bump CreateHttpRequestMessage(..., IRequest request)

--- src/Daktela.HttpClient/Implementations/DaktelaHttpClient.cs
+++ src/Daktela.HttpClient/Implementations/DaktelaHttpClient.cs
@@ -1,8 +1,11 @@
+using Daktela.HttpClient.Api.Responses;
 using Daktela.HttpClient.Interfaces;
 using System;
+using System.Collections.Specialized;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Daktela.HttpClient.Implementations;
 
@@ -29,7 +32,9 @@ public class DaktelaHttpClient : IDaktelaHttpClient
         }
     }
 
-    public async Task<T> GetAsync<T>(IHttpResponseParser httpResponseParser, string uri, CancellationToken cancellationToken)
+    public async Task<SingleResponse<T>> GetAsync<T>(
+        IHttpResponseParser httpResponseParser, string uri, CancellationToken cancellationToken
+    ) where T : class
     {
         var uriObject = new Uri(uri, UriKind.Relative);
 
@@ -39,7 +44,27 @@ public class DaktelaHttpClient : IDaktelaHttpClient
             .SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
             .ConfigureAwait(false);
 
-        var response = await httpResponseParser.ParseResponseAsync<T>(httpResponse.Content, cancellationToken);
+        var response = await httpResponseParser.ParseResponseAsync<SingleResponse<T>>(httpResponse.Content, cancellationToken);
+
+        return response;
+    }
+
+    public async Task<ListResponse<T>> GetListAsync<T>(
+        IHttpResponseParser httpResponseParser,
+        string uri,
+        IRequest request,
+        CancellationToken cancellationToken
+    ) where T : class
+    {
+        var uriObject = new Uri(uri, UriKind.Relative);
+
+        using var httpRequestMessage = CreateHttpRequestMessage(HttpMethod.Get, uriObject, request);
+
+        var httpResponse = await _httpClient
+            .SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+            .ConfigureAwait(false);
+
+        var response = await httpResponseParser.ParseResponseAsync<ListResponse<T>>(httpResponse.Content, cancellationToken);
 
         return response;
     }
@@ -47,6 +72,47 @@ public class DaktelaHttpClient : IDaktelaHttpClient
     private HttpRequestMessage CreateHttpRequestMessage(
         HttpMethod method, Uri uri
     ) => new(method, uri);
+
+    private HttpRequestMessage CreateHttpRequestMessage(
+        HttpMethod method, Uri uri, IRequest request
+    )
+    {
+        var queryDictionary = new NameValueCollection();
+        if (request is ISortableQuery { Sorting: { } sorting })
+        {
+            // TODO sorting
+        }
+
+        if (request is IFilteringQuery { Filters: { } filters })
+        {
+            // TODO filters
+        }
+
+        if (request is IPagedQuery { Paging: { } paging })
+        {
+            queryDictionary.Add("skip", paging.Skip.ToString());
+            queryDictionary.Add("take", paging.Take.ToString());
+        }
+
+        if (queryDictionary.HasKeys())
+        {
+            var parsedQuery = HttpUtility.ParseQueryString(uri.Query);
+
+            foreach (string key in queryDictionary.Keys)
+            {
+                parsedQuery[key] = queryDictionary[key];
+            }
+
+            var uriBuilder = new UriBuilder(uri)
+            {
+                Query = parsedQuery.ToString()
+            };
+
+            uri = uriBuilder.Uri;
+        }
+
+        return CreateHttpRequestMessage(method, uri);
+    }
 
     private HttpRequestMessage CreateHttpRequestMessage<TBody>(
         HttpMethod method, Uri uri, TBody? body
