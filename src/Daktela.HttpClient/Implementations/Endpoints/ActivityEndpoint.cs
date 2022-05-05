d3b6c373ad210bf17fe11d65bb43f7c64bb2046c
Jan Trejbal <jan@trejbal.land>
2022-05-05 12:23:10 +0200

    Add R + U + List endpoints for activities

--- src/Daktela.HttpClient/Implementations/Endpoints/ActivityEndpoint.cs
+++ src/Daktela.HttpClient/Implementations/Endpoints/ActivityEndpoint.cs
@@ -1,8 +1,13 @@
 using Daktela.HttpClient.Api.Tickets;
 using Daktela.HttpClient.Interfaces;
 using Daktela.HttpClient.Interfaces.Endpoints;
+using Daktela.HttpClient.Interfaces.Requests;
+using Daktela.HttpClient.Interfaces.Requests.Options;
+using Daktela.HttpClient.Interfaces.ResponseBehaviours;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Daktela.HttpClient.Implementations.Endpoints;
 
@@ -11,17 +16,64 @@ public class ActivityEndpoint : IActivityEndpoint
     private readonly IDaktelaHttpClient _daktelaHttpClient;
     private readonly IHttpRequestSerializer _httpRequestSerializer;
     private readonly IHttpResponseParser _httpResponseParser;
+    private readonly IPagedResponseProcessor<IActivityEndpoint> _pagedResponseProcessor;
 
     public ActivityEndpoint(
         IDaktelaHttpClient daktelaHttpClient,
         IHttpRequestSerializer httpRequestSerializer,
-        IHttpResponseParser httpResponseParser
+        IHttpResponseParser httpResponseParser,
+        IPagedResponseProcessor<IActivityEndpoint> pagedResponseProcessor
     )
     {
         _daktelaHttpClient = daktelaHttpClient;
         _httpRequestSerializer = httpRequestSerializer;
         _httpResponseParser = httpResponseParser;
+        _pagedResponseProcessor = pagedResponseProcessor;
     }
+
+    public async Task<ReadActivity> GetActivityAsync(
+        string name, CancellationToken cancellationToken
+    )
+    {
+        var encodedName = HttpUtility.UrlEncode(name);
+
+        var contact = await _daktelaHttpClient.GetAsync<ReadActivity>(
+            _httpResponseParser,
+            $"{IActivityEndpoint.UriPrefix}/{encodedName}{IActivityEndpoint.UriPostfix}",
+            cancellationToken
+        ).ConfigureAwait(false);
+
+        return contact.Result;
+    }
+
+    public IAsyncEnumerable<ReadActivity> GetActivitiesAsync(
+        IRequest request,
+        IRequestOption requestOption,
+        IResponseBehaviour responseBehaviour,
+        CancellationToken cancellationToken
+    ) => _pagedResponseProcessor.InvokeAsync(
+        request,
+        requestOption,
+        responseBehaviour,
+        new
+        {
+            daktelaHttpClient = _daktelaHttpClient,
+            httpResponseParser = _httpResponseParser
+        },
+        async static (
+            request,
+            _,
+            _,
+            ctx,
+            cancellationToken
+        ) => await ctx.daktelaHttpClient.GetListAsync<ReadActivity>(
+            ctx.httpResponseParser,
+            $"{IActivityEndpoint.UriPrefix}{IActivityEndpoint.UriPostfix}",
+            request,
+            cancellationToken
+        ),
+        cancellationToken
+    ).IteratingConfigureAwait(cancellationToken);
 
     public async Task<ReadActivity> CreateActivityAsync(
         CreateActivity activity, CancellationToken cancellationToken
@@ -32,4 +84,21 @@ public class ActivityEndpoint : IActivityEndpoint
         activity,
         cancellationToken
     ).ConfigureAwait(false);
+
+    public async Task<ReadActivity> UpdateActivityAsync(
+        string name,
+        UpdateActivity contact,
+        CancellationToken cancellationToken
+    )
+    {
+        var encodedName = HttpUtility.UrlEncode(name);
+
+        return await _daktelaHttpClient.PutAsync<UpdateActivity, ReadActivity>(
+            _httpRequestSerializer,
+            _httpResponseParser,
+            $"{IActivityEndpoint.UriPrefix}/{encodedName}{IActivityEndpoint.UriPostfix}",
+            contact,
+            cancellationToken
+        ).ConfigureAwait(false);
+    }
 }
--- src/Daktela.HttpClient/Interfaces/Endpoints/IActivityEndpoint.cs
+++ src/Daktela.HttpClient/Interfaces/Endpoints/IActivityEndpoint.cs
@@ -1,4 +1,8 @@
 using Daktela.HttpClient.Api.Tickets;
+using Daktela.HttpClient.Interfaces.Requests;
+using Daktela.HttpClient.Interfaces.Requests.Options;
+using Daktela.HttpClient.Interfaces.ResponseBehaviours;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,7 +13,26 @@ public interface IActivityEndpoint
     protected internal const string UriPrefix = "/api/v6/activities";
     protected internal const string UriPostfix = ".json";
 
+    Task<ReadActivity> GetActivityAsync(
+        string name,
+        CancellationToken cancellationToken = default
+    );
+
+    IAsyncEnumerable<ReadActivity> GetActivitiesAsync(
+        IRequest request,
+        IRequestOption requestOption,
+        IResponseBehaviour responseBehaviour,
+        CancellationToken cancellationToken = default
+    );
+
     Task<ReadActivity> CreateActivityAsync(
-        CreateActivity ticket, CancellationToken cancellationToken
+        CreateActivity ticket,
+        CancellationToken cancellationToken = default
+    );
+
+    Task<ReadActivity> UpdateActivityAsync(
+        string name,
+        UpdateActivity ticket,
+        CancellationToken cancellationToken = default
     );
 }
