ea38fb7c3733f8df36f2e64c5cec4db0934f648c
Jan Trejbal <jan.trejbal@aviationexam.com>
2022-06-28 14:25:48 +0200

    Introduce non-generic BadRequestException

--- src/Daktela.HttpClient/Exceptions/BadRequestException.cs
+++ src/Daktela.HttpClient/Exceptions/BadRequestException.cs
@@ -2,16 +2,23 @@ using Daktela.HttpClient.Api.Responses.Errors;
 
 namespace Daktela.HttpClient.Exceptions;
 
-public class BadRequestException<TContract> : DaktelaException
+public class BadRequestException : DaktelaException
+{
+    public IErrorResponse ErrorsResponse { get; }
+
+    protected BadRequestException(IErrorResponse errorsResponse)
+    {
+        ErrorsResponse = errorsResponse;
+    }
+}
+
+public class BadRequestException<TContract> : BadRequestException
     where TContract : class
 {
     public TContract Contract { get; }
 
-    public IErrorResponse ErrorsResponse { get; }
-
-    public BadRequestException(TContract contract, IErrorResponse errorsResponse)
+    public BadRequestException(TContract contract, IErrorResponse errorsResponse) : base(errorsResponse)
     {
         Contract = contract;
-        ErrorsResponse = errorsResponse;
     }
 }
