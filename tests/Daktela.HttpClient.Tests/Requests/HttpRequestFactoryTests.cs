bfaba013cce024422c25e3016c028b02f5577ce7
Jan Trejbal <jan@trejbal.land>
2024-11-18 22:05:07 +0100

    Update HttpRequestFactoryTests

--- tests/Daktela.HttpClient.Tests/Requests/HttpRequestFactoryTests.cs
+++ tests/Daktela.HttpClient.Tests/Requests/HttpRequestFactoryTests.cs
@@ -44,7 +44,11 @@ public class HttpRequestFactoryTests
 
         Assert.Equal(HttpMethod.Get, httpRequestMessage.Method);
         Assert.NotNull(httpRequestMessage.RequestUri);
+#if NET9_0_OR_GREATER
+        Assert.Equal($"{DaktelaUrl}/{DaktelaContractPath}?fields%5b0%5d=name&accessToken={AccessToken}", httpRequestMessage.RequestUri!.ToString());
+#else
         Assert.Equal($"{DaktelaUrl}/{DaktelaContractPath}?fields[0]=name&accessToken={AccessToken}", httpRequestMessage.RequestUri!.ToString());
+#endif
     }
 
     [Fact]
@@ -72,7 +76,11 @@ public class HttpRequestFactoryTests
 
         Assert.Equal(HttpMethod.Get, httpRequestMessage.Method);
         Assert.NotNull(httpRequestMessage.RequestUri);
+#if NET9_0_OR_GREATER
+        Assert.Equal($"{DaktelaUrl}/{DaktelaContractPath}?sort%5b0%5d%5bdir%5d=asc&sort%5b0%5d%5bfield%5d=edited&accessToken={AccessToken}", httpRequestMessage.RequestUri!.ToString());
+#else
         Assert.Equal($"{DaktelaUrl}/{DaktelaContractPath}?sort[0][dir]=asc&sort[0][field]=edited&accessToken={AccessToken}", httpRequestMessage.RequestUri!.ToString());
+#endif
     }
 
     [Fact]
@@ -86,10 +94,17 @@ public class HttpRequestFactoryTests
 
         Assert.Equal(HttpMethod.Get, httpRequestMessage.Method);
         Assert.NotNull(httpRequestMessage.RequestUri);
+#if NET9_0_OR_GREATER
+        Assert.Equal(
+            $"{DaktelaUrl}/{DaktelaContractPath}?filter%5bfield%5d=contact&filter%5boperator%5d=eq&filter%5bvalue%5d=a+value&accessToken={AccessToken}",
+            httpRequestMessage.RequestUri!.ToString()
+        );
+#else
         Assert.Equal(
             $"{DaktelaUrl}/{DaktelaContractPath}?filter[field]=contact&filter[operator]=eq&filter[value]=a+value&accessToken={AccessToken}",
             httpRequestMessage.RequestUri!.ToString()
         );
+#endif
     }
 
     [Fact]
@@ -103,10 +118,17 @@ public class HttpRequestFactoryTests
 
         Assert.Equal(HttpMethod.Get, httpRequestMessage.Method);
         Assert.NotNull(httpRequestMessage.RequestUri);
+#if NET9_0_OR_GREATER
+        Assert.Equal(
+            $"{DaktelaUrl}/{DaktelaContractPath}?filter%5bfield%5d=contact&filter%5boperator%5d=eq&filter%5bvalue%5d=a%2bvalue&accessToken={AccessToken}",
+            httpRequestMessage.RequestUri!.ToString()
+        );
+#else
         Assert.Equal(
             $"{DaktelaUrl}/{DaktelaContractPath}?filter[field]=contact&filter[operator]=eq&filter[value]=a%2bvalue&accessToken={AccessToken}",
             httpRequestMessage.RequestUri!.ToString()
         );
+#endif
     }
 
     [Fact]
@@ -123,6 +145,15 @@ public class HttpRequestFactoryTests
 
         Assert.Equal(HttpMethod.Get, httpRequestMessage.Method);
         Assert.NotNull(httpRequestMessage.RequestUri);
+#if NET9_0_OR_GREATER
+        Assert.Equal(
+            $"{DaktelaUrl}/{DaktelaContractPath}?filter%5blogic%5d=and"
+            + "&filter%5bfilters%5d%5b0%5d%5bfield%5d=name&filter%5bfilters%5d%5b0%5d%5boperator%5d=eq&filter%5bfilters%5d%5b0%5d%5bvalue%5d=Johan"
+            + "&filter%5bfilters%5d%5b1%5d%5bfield%5d=email&filter%5bfilters%5d%5b1%5d%5boperator%5d=endswith&filter%5bfilters%5d%5b1%5d%5bvalue%5d=%40gmail.com"
+            + $"&accessToken={AccessToken}",
+            httpRequestMessage.RequestUri!.ToString()
+        );
+#else
         Assert.Equal(
             $"{DaktelaUrl}/{DaktelaContractPath}?filter[logic]=and"
             + "&filter[filters][0][field]=name&filter[filters][0][operator]=eq&filter[filters][0][value]=Johan"
@@ -130,5 +161,6 @@ public class HttpRequestFactoryTests
             + $"&accessToken={AccessToken}",
             httpRequestMessage.RequestUri!.ToString()
         );
+#endif
     }
 }
